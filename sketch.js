88ec32d0868f3194ac0dd572db80791f53db55df
Cmsoll <sophiamariano22@gmail.com>
2024-08-20 16:58:28 -0300

    salvando projetinho

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+    createCanvas(600, 400);
+  }
+  
+  function draw() {
+    background(`#F3DA02`);
+    stroke(`rgb(0,0,1)`);
+          
+    rect (mouseX,mouseY,20,25,)
+  }
+  function setup() {
+    createCanvas(400, 400);
+    background("#03A9F4")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("rgb(255,0,162)");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+
+  
